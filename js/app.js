643cc615579dc523e9f29b507cda759a8199a201
Saite <saiteways@gmail.com>
2023-10-09 06:37:28 +1000

    Подключены библиотеки

---
+++ js/app.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', e => {
+    document.body.style.cssText += `--scrollTop: ${this.scrollY}px` // Update method
+})
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create({
+    wrapper: '.wrapper',
+    content: '.content'
+})
